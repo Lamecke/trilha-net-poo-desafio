67b4d1b6443fdc3b19bec2a42501675e6fe36b5a
Lamecke <lamecke_bruno@hotmail.com>
2024-08-07 15:54:59 -0300

    todo: test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("123", "n2", "1332356", 2);
+Iphone iphone = new Iphone("183", "7", "2332351", 4);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("minecraft");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("minecraft");
